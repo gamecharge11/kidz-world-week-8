38b50ce14618fe2b59d1e857349d1b3ecc7300e8
gamecharge11 <73420074+gamecharge11@users.noreply.github.com>
2020-12-19 13:43:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var cardValue = document.getElementById("cart-value");
+var cartButton = document.getElementById("cart");
+
+var book1Add = document.getElementById("book-1");
+var book2Add = document.getElementById("book-2");
+var book3Add = document.getElementById("book-3");
+var book4Add = document.getElementById("book-4");
+var book5Add = document.getElementById("book-5");
+var book6Add = document.getElementById("book-6");
+
+var game1Add = document.getElementById("game-1");
+var game2Add = document.getElementById("game-2");
+var game3Add = document.getElementById("game-3");
+var game4Add = document.getElementById("game-4");
+
+var craft1Add = document.getElementById("craft-1");
+var craft2Add = document.getElementById("craft-2");
+var craft3Add = document.getElementById("craft-3");
+var craft4Add = document.getElementById("craft-4");
+
+console.log(book1Add)
